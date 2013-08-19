d3bbc93718dca14a00acea6751f866545c3fe607
Andrew Stanton <Refefer@gmail.com>
2013-08-19 09:49:29 -0400

    Ups version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='python-cts',
-      version="0.1.1",
+      version="0.1.2",
       description='Command line ctag search',
       license="LICENSE",
       scripts=['bin/cts'],
