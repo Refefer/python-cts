0da02f605238f3c1d261a12dbcc680e65c7d4751
Andrew Stanton <Refefer@gmail.com>
2013-08-20 12:37:38 -0400

    Create a non-borked dist

--- setup.py
+++ setup.py
@@ -2,18 +2,14 @@
 
 from setuptools import setup
 
-with open('README.md') as file:
-    long_description = file.read()
-
 setup(name='python-cts',
-      version="0.1.5.1",
+      version="0.1.5.2",
       description='Command line ctag search',
       url="https://github.com/Refefer/python-cts",
       license="LICENSE",
       scripts=['bin/cts'],
       author='Andrew Stanton',
       author_email='Andrew Stanton',
-      long_description = long_description,
       install_requires=['python-ctags'],
       categories=[
        "License :: OSI Approved :: Apache Software License",
