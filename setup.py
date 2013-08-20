b1f4c733f9d8cd8ab66a5dbf54614a049f5d7841
Andrew Stanton <Refefer@gmail.com>
2013-08-20 12:26:20 -0400

    README

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@
 
 from setuptools import setup
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(name='python-cts',
-      version="0.1.5",
+      version="0.1.5.1",
       description='Command line ctag search',
       url="https://github.com/Refefer/python-cts",
       license="LICENSE",
       scripts=['bin/cts'],
       author='Andrew Stanton',
       author_email='Andrew Stanton',
-      install_requires=['python-ctags'])
+      long_description = long_description,
+      install_requires=['python-ctags'],
+      categories=[
+       "License :: OSI Approved :: Apache Software License",
+       "Programming Language :: Python :: 2.6",
+       "Operating System :: OS Independent"
+      ])
