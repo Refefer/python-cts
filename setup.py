52a79489c0d303111abaecd95f06430007ad3593
Andrew Stanton <Refefer@gmail.com>
2013-08-20 11:51:53 -0400

    Adds url, updates version

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from setuptools import setup
 
 setup(name='python-cts',
-      version="0.1.4",
+      version="0.1.5",
       description='Command line ctag search',
+      url="https://github.com/Refefer/python-cts",
       license="LICENSE",
       scripts=['bin/cts'],
       author='Andrew Stanton',
